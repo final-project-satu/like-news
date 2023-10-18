8c1377f733eb4799a54ef348182cf8df0726ec50
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2023-10-18 15:39:30 +0700

    setup project like news

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      {/* Header */}
+      {/* Navbar */}
+      <main>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
