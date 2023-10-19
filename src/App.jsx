93f4214f4d02974bfad831a9cddbff306c5e57d1
zaki fauzan <zakifauzanrabb@gmail.com>
2023-10-19 15:27:00 +0800

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import IndonesiaPage from './pages/indonesia';
 import ErrorPage from './pages/404';
+import Navbars from './components/organisms/Navbar';
+import Headers from './components/organisms/Headers';
 
 function App() {
   return (
     <>
-      {/* Header */}
-      {/* Navbar */}
+      <Headers/>
+      <Navbars/> 
       <main className="px-2 md:px-20 mb-10">
         <Routes>
           <Route path="/" element={<IndonesiaPage />} />
---
+++ src/components/organisms/Headers.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+
+export default function Headers(){
+    return(
+    <>
+        <div className="bg-headers">
+            <div className="text-center">
+                <div className="text-headers">Hai selamat datang</div>
+                <Button variant="outline-light" className="btn-headers">Start Read</Button>
+            </div>
+        </div>
+    </>
+    )
+}
