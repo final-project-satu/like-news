35a0a72fd532b9cd6a83373c8672822d6e2a4222
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2023-10-22 15:37:48 +0700

    add feature search

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -4,26 +4,56 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import LOGO from '../../assets/img/logo-black.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
 
 function Navbars() {
+  const [keywords, setKeywords] = React.useState('');
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const handleSearchKeywords = (e) => {
+    e.preventDefault();
+    if (!keywords) {
+      window.location.href = currentPath;
+    } else {
+      window.location.href = `/search/${keywords}`;
+    }
+  };
+
   return (
-    <Navbar expand="lg" sticky='top'  variant='light' className="nav">
+    <Navbar expand="lg" sticky="top" variant="light" className="nav">
       <Container fluid>
         <Navbar.Brand href="#">
           <img src={LOGO} alt="logo like news" className="w-48 h-10 object-cover " />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 text-lg "navbarScroll>
-            <Nav.Link><Link to='/'>Indonesia</Link></Nav.Link>
-            <Nav.Link><Link to='/programming'>Programming</Link></Nav.Link>
-            <Nav.Link><Link to='/covid'>Covid-19</Link></Nav.Link>
-            <Nav.Link><Link to='/saved'>Saved</Link></Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0 text-lg " navbarScroll>
+            <Nav.Link>
+              <Link to="/indonesia">Indonesia</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/programming">Programming</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/covid">Covid-19</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/saved">Saved</Link>
+            </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-            <Button variant="outline-success">Search</Button>
+          <Form className="d-flex" onSubmit={handleSearchKeywords}>
+            <Form.Control
+              type="search"
+              placeholder="Cari Berita Terbaru"
+              className="me-2"
+              aria-label="Search"
+              onChange={(e) => setKeywords(e.target.value)}
+            />
+            <Button variant="outline-success" type="submit">
+              Search
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
---
+++ src/pages/search.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Card from '../components/organisms/Card';
+import { getNewsSearch } from '../services/news.service';
+import CardSkeleton from '../components/organisms/CardSkeleton';
+
+const SearchPage = () => {
+  const [newsSearch, setNewsSearch] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isError, setIsError] = React.useState(false);
+  const { keyword } = useParams();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getNewsSearch(keyword);
+        setNewsSearch(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [keyword]);
+
+  if (isError) {
+    return <div>Gagal Menampilkan data...</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center my-5">{keyword}</h2>
+
+      <div className="w-full grid gap-5 lg:grid-cols-3">
+        {isLoading ? (
+          <>
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+          </>
+        ) : (
+          <>
+            {newsSearch.map((article, idx) => (
+              <div key={`${article?.title}-${idx}`} className="border-[1px] border-slate-600 p-3">
+                <Card data={article} />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
