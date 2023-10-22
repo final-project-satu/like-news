9550acefd02cec73c6634f1ad2d142a638334e8f
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2023-10-22 15:38:27 +0700

    add covid page

---
+++ src/pages/covid.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { getNews } from '../services/news.service';
+import CardSkeleton from '../components/organisms/CardSkeleton';
+import Card from '../components/organisms/Card';
+
+const CovidPage = () => {
+  const [newsProgramming, setNewsProgramming] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getNews('covid');
+        setNewsProgramming(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isError) {
+    return <div>Gagal Menampilkan data...</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center my-5">Covid-19</h2>
+
+      <div className="w-full grid gap-5 lg:grid-cols-3 md:grid-cols-2">
+        {isLoading ? (
+          <>
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+          </>
+        ) : (
+          <>
+            {newsProgramming.map((article, idx) => (
+              <div key={`${article?.title}-${idx}`} className="border-[1px] border-slate-600 p-3">
+                <Card data={article} />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CovidPage;
